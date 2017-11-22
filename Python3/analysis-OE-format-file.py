d83a37fbab0ce1587596d7cf7193a9840606afcc
chinmayeelm <chinmayeelm@gmail.com>
2017-11-22 15:04:08 +0530

    filtfilt instead of lfilter

--- Python3/analysis-OE-format-file.py
+++ Python3/analysis-OE-format-file.py
@@ -3,50 +3,33 @@
 Code to analyse Open-ephys recording data stored in Open-ephys file format
 """
 import OpenEphys
-from scipy.signal import butter, lfilter
+# from scipy.signal import butter, lfilter
+import scipy
 import pyqtgraph as pg
 import matplotlib.pyplot as plt
 from pyqtgraph.Qt import QtGui
-
-
-""" Creates bandpass filter """
-
-
-def butter_bandpass(lowcut, highcut, fs, order=5):
-    nyq = 0.5 * fs
-    low = lowcut / nyq
-    high = highcut / nyq
-    b, a = butter(order, [low, high], btype='bandpass', analog=False)
-    return b, a
-
-
-""" Uses the above filter to give filtered data """
-
-
-def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
-    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
-    y = lfilter(b, a, data)
-    return y
+import numpy as np
 
 
 rec = OpenEphys.loadContinuous(
     "/home/chetana/Documents/Work/OEdata/2017-11-01_04-11-56/100_CH1.continuous")
 
-fs = 30000.0
-lowcut = 150.0
-highcut = 1500.0
 
-filtered_data = butter_bandpass_filter(
-    rec['data'], lowcut, highcut, fs, order=6)
+order = 5
+fs = 30000.0
+nyq = 0.5 * fs
+lowcut = 150.0/nyq
+highcut = 1000.0/nyq
+
+b, a = scipy.signal.butter(
+    order, [lowcut, highcut], btype='bandpass', analog=False, output='ba')
+filtered_data = scipy.signal.filtfilt(b, a, rec['data'])
 
 #plt.xlabel('time (seconds)')
-#plt.hlines([-a, a], 0, T, linestyles='--')
 # plt.grid(True)
 # plt.axis('tight')
 #plt.legend(loc='upper left')
-
 # plt.show()
-
 # axis.setLabel('left', "Y Axis", units='uV')
 # axis.setLabel('bottom', "X Axis", units='time')
 
