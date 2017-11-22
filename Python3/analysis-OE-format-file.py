dde6711b0be6558aad38b89662ac3d1cdbdff267
chinmayeelm <chinmayeelm@gmail.com>
2017-11-22 10:01:05 +0530

    Filter and plotting

--- Python3/analysis-OE-format-file.py
+++ Python3/analysis-OE-format-file.py
@@ -1,7 +1,15 @@
+"""Written by Chinmayee.
+
+Code to analyse Open-ephys recording data stored in Open-ephys file format
+"""
 import OpenEphys
 from scipy.signal import butter, lfilter
 import pyqtgraph as pg
 import matplotlib.pyplot as plt
+from pyqtgraph.Qt import QtGui
+
+
+""" Creates bandpass filter """
 
 
 def butter_bandpass(lowcut, highcut, fs, order=5):
@@ -12,6 +20,9 @@ def butter_bandpass(lowcut, highcut, fs, order=5):
     return b, a
 
 
+""" Uses the above filter to give filtered data """
+
+
 def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
     b, a = butter_bandpass(lowcut, highcut, fs, order=order)
     y = lfilter(b, a, data)
@@ -19,14 +30,15 @@ def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
 
 
 rec = OpenEphys.loadContinuous(
-    "~/Documents/Work/OEdata/2017-11-01_04-11-56/100_CH1.continuous")
+    "/home/chetana/Documents/Work/OEdata/2017-11-01_04-11-56/100_CH1.continuous")
 
 fs = 30000.0
 lowcut = 150.0
 highcut = 1500.0
 
-y = butter_bandpass_filter(rec['data'], lowcut, highcut, fs, order=6)
-pg.plot(y, title='Filtered signal')
+filtered_data = butter_bandpass_filter(
+    rec['data'], lowcut, highcut, fs, order=6)
+
 #plt.xlabel('time (seconds)')
 #plt.hlines([-a, a], 0, T, linestyles='--')
 # plt.grid(True)
@@ -35,8 +47,25 @@ pg.plot(y, title='Filtered signal')
 
 # plt.show()
 
-pg.plot(rec['data'], title='raw data')
-#axis.setLabel('left', "Y Axis", units='uV')
-#axis.setLabel('bottom', "X Axis", units='time')
+# axis.setLabel('left', "Y Axis", units='uV')
+# axis.setLabel('bottom', "X Axis", units='time')
 
-pg.QtGui.QApplication.exec_()
+
+app = QtGui.QApplication([])
+# win = pg.GraphicsWindow(title="Response to tactile stimulus on cephalic hair")
+# win.resize(1000, 600)
+# win.setWindowTitle('Suction electrode recording from VNC')
+pg.setConfigOptions(antialias=True)
+# p1 = win.addPlot(time, filtered_data, title="Suction electrode recording from VNC")
+p1 = pg.plot(filtered_data,
+             title="Suction electrode recording from VNC (filtered)")
+p1.setLabel('left', "Y Axis", units='uV')
+p1.setLabel('bottom', "X Axis", units='time')
+
+p2 = pg.plot(
+    rec['data'], title="Suction electrode recording from VNC (Raw data)")
+p2.setLabel('left', "Y Axis", units='uV')
+p2.setLabel('bottom', "X Axis", units='time')
+
+
+QtGui.QApplication.instance().exec_()
