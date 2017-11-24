e7385a3b3555bd9a4b7ff2250c92be9b29197fc6
chinmayeelm <chinmayeelm@gmail.com>
2017-11-24 09:06:44 +0530

    plotting with matplotlib.pyplot. Time axis not shown. timestamps and samples length mismatch

--- Python3/analysis-OE-format-file.py
+++ Python3/analysis-OE-format-file.py
@@ -3,12 +3,13 @@
 Code to analyse Open-ephys recording data stored in Open-ephys file format
 """
 import OpenEphys
-# from scipy.signal import butter, lfilter
+from scipy import signal
 import scipy
 import pyqtgraph as pg
 import matplotlib.pyplot as plt
 from pyqtgraph.Qt import QtGui
 import numpy as np
+import matplotlib as mpl
 
 
 rec = OpenEphys.loadContinuous(
@@ -25,6 +26,13 @@ b, a = scipy.signal.butter(
     order, [lowcut, highcut], btype='bandpass', analog=False, output='ba')
 filtered_data = scipy.signal.filtfilt(b, a, rec['data'])
 
+a = float(rec['header']['sampleRate'])
+print (rec['header']['sampleRate'])
+print type(rec['timestamps'])
+time = a * rec['timestamps']
+print time
+
+
 #plt.xlabel('time (seconds)')
 # plt.grid(True)
 # plt.axis('tight')
@@ -33,7 +41,7 @@ filtered_data = scipy.signal.filtfilt(b, a, rec['data'])
 # axis.setLabel('left', "Y Axis", units='uV')
 # axis.setLabel('bottom', "X Axis", units='time')
 
-
+'''
 app = QtGui.QApplication([])
 # win = pg.GraphicsWindow(title="Response to tactile stimulus on cephalic hair")
 # win.resize(1000, 600)
@@ -49,6 +57,26 @@ p2 = pg.plot(
     rec['data'], title="Suction electrode recording from VNC (Raw data)")
 p2.setLabel('left', "Y Axis", units='uV')
 p2.setLabel('bottom', "X Axis", units='time')
+'''
+
+label_size = 15
+mpl.rcParams['xtick.labelsize'] = label_size
+mpl.rcParams['ytick.labelsize'] = label_size
+
+plt.figure("Suction electrode recording from VNC(filtered)")
+plt.plot(filtered_data)
+# plt.title("Suction electrode recording from VNC(filtered)", fontsize=20)
+plt.ylabel('LFP (uV)', fontsize=20)
+plt.xlabel('time', fontsize=20)
+plt.xticks([])
+
+plt.figure("Suction electrode recording from VNC(raw plot)")
+plt.plot(rec['data'])
+# plt.title("Suction electrode recording from VNC", fontsize=20)
+plt.ylabel('LFP (uV)', fontsize=20)
+plt.xlabel('time', fontsize=20)
+plt.xticks([])
+plt.show()
 
 
-QtGui.QApplication.instance().exec_()
+# QtGui.QApplication.instance().exec_()
