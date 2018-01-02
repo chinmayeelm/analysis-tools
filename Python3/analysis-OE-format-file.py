b80fce32a37171ec6658e83997e132744ee1ea15
chinmayeelm <chinmayeelm@gmail.com>
2018-01-02 14:31:11 +0530

    plot using matplotlib.pyplot

--- Python3/analysis-OE-format-file.py
+++ Python3/analysis-OE-format-file.py
@@ -13,7 +13,7 @@ import matplotlib as mpl
 
 
 rec = OpenEphys.loadContinuous(
-    "/home/chetana/Documents/Work/OEdata/2017-11-01_04-11-56/100_CH1.continuous")
+    "/home/chetana/Documents/Work/OEdata/2017-11-01_CH/100_CH1.continuous")
 
 
 order = 5
@@ -29,9 +29,14 @@ filtered_data = scipy.signal.filtfilt(b, a, rec['data'])
 a = float(rec['header']['sampleRate'])
 print (rec['header']['sampleRate'])
 print type(rec['timestamps'])
-time = a * rec['timestamps']
+time = rec['timestamps']
 print time
+total_recording_time = float(len(filtered_data))/float(rec['header']['sampleRate'])
+#total_recording_time_ = np.ceil(total_ recording_time)
 
+t = np.linspace(time[0], time[-1], 5)
+#t = str(t)
+#print t
 
 #plt.xlabel('time (seconds)')
 # plt.grid(True)
@@ -66,9 +71,11 @@ mpl.rcParams['ytick.labelsize'] = label_size
 plt.figure("Suction electrode recording from VNC(filtered)")
 plt.plot(filtered_data)
 # plt.title("Suction electrode recording from VNC(filtered)", fontsize=20)
+#plt.plot(t*float(rec['header']['sampleRate']))
 plt.ylabel('LFP (uV)', fontsize=20)
 plt.xlabel('time', fontsize=20)
 plt.xticks([])
+
 
 plt.figure("Suction electrode recording from VNC(raw plot)")
 plt.plot(rec['data'])
@@ -76,7 +83,7 @@ plt.plot(rec['data'])
 plt.ylabel('LFP (uV)', fontsize=20)
 plt.xlabel('time', fontsize=20)
 plt.xticks([])
+
+
 plt.show()
-
-
 # QtGui.QApplication.instance().exec_()
